6862bc9d2da2c00398f3537ccedd69c7a82a7397
MelihOZKAS <melihozkass@gmail.com>
2024-05-05 00:11:59 +0300

    ALLAH ım utandırma

--- Seo/models.py
+++ Seo/models.py
@@ -147,7 +147,7 @@ class Kontrol(models.Model):
     Akibeti = models.CharField(max_length=255, choices=kontrol, null=True, blank=True)
     Kaynak_Dili = models.CharField(max_length=255, choices=Dili, null=True, blank=True)
     Kaynak_Linki = models.URLField(blank=True, null=True)
-    Kaynak_Ana_link = models.ForeignKey(BackEndDomain, blank=True, on_delete=models.SET_NULL)
+    Kaynak_Ana_link = models.ForeignKey(BackEndDomain, blank=True, null=True, on_delete=models.SET_NULL)
     olusturma_tarihi = models.DateTimeField(auto_now_add=True)
     cocukmasali = models.CharField(max_length=255, choices=islem, null=True, blank=True, default="Kontrol")
     kidsStories = models.CharField(max_length=255, choices=islem, null=True, blank=True, default="Kontrol")
