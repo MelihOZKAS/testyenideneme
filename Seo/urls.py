e57a47e87ae9ba10c314e3ec045b471cc47e45e5
MelihOZKAS <melihozkass@gmail.com>
2024-05-05 11:15:56 +0300

    ALLAH ım utandırma

--- Seo/urls.py
+++ Seo/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("mahsul-listesi-cek/", views.mahsullistesicek),
     path("add-post/", views.post_add),
     path("mahsul-cek/", views.mahsulcek),
-    path("ai-cek/", views.aiPostCek),
+    path("ai-cek/", views.get_random_kontrol),
     path('story-preview/<slug:slug>/', views.StoryPreviewView.as_view(), name='story_preview'),
     path('<str:post_slug>/', views.Enderun, name='post-getir'),#Blog Git
     path('new/<str:post_slug>/', views.EnderunNew, name='post-getir-new'),#Blog Git
